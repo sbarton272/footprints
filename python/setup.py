1459db0f6bd42b096bcb2f1aa8b099170b126f01
Robert Kotcher <rkotcher@gmail.com>
2013-09-08 16:03:13 -0400

    a few simple things

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+'''from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+  readme = f.read()
+
+setup(
+  name='footprints',
+  version='0.0.1',
+  description='project 1, SIS: Hybrid instrument building',
+  long_description=readme,
+  author='Robert Kotcher, Spencer Barton',
+  author_email='rkotcher@andrew.cmu.edu, sebarton@andrew.cmu.edu',
+  url=https://github.com/sbarton272/footprints,
+  license=license,
+  packages=find_packages(exclude=('tests', 'docs'))
+)'''
